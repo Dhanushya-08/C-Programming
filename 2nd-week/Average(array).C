c2d826f3dfde24dd84f8954ef54f1e89400dd215
J.Aron Dhanushya <arondhanushya2006@gmail.com>
2024-09-22 19:33:07 +0530

    Add files via upload

---
+++ 2nd-week/Average(array).C
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+  int i,n,marks[40],total=0,avg;
+  clrscr();
+  for(i=1;i<=40;i++)
+  {
+    printf("Enter Student's mark:");
+    scanf("%d",& marks[i]);
+    total=total+marks[i];
+  }
+  avg=total/40;
+  printf("The average mark is:%d",avg);
+getch();
+}
